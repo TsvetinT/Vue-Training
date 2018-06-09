2c9be6f5485129851a2a27e9da688fc1a31dfb32
TST <todorov.tsvetin@gmail.com>
2018-06-09 15:49:41 +0300

    Initial commit

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,7 +1,7 @@
 <template >
   <v-container fluid grid-list-md>
     <v-layout row wrap>
-      <v-flex v-for="card in cards" v-bind="{ [`xs${card.flex}`]: true }" :key="card.title">
+      <v-flex v-for="(card, index) in cards" v-bind="{ [`xs${card.flex}`]: true }" :key="card.title">
         <v-card>
           <v-card-media :src="card.src" height="200px">
             <v-container fill-height fluid>
@@ -14,8 +14,8 @@
           </v-card-media>
           <v-card-actions>
 
-            <v-btn outline flat color="orange" @click.native="show = !show">Explore</v-btn>
-            <v-card-text v-show="show">
+            <v-btn outline flat color="orange" @click="showInformation(index)">Explore</v-btn>
+            <v-card-text class="hidden" :ref="`cardText${index}`">
               I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
             </v-card-text>
 
@@ -50,8 +50,21 @@ export default {
       { title: 'Honey', src: require('@/assets/beer3.jpg'), flex: 4 },
       { title: 'Strong', src: require('@/assets/beer3.jpg'), flex: 4 }
     ]
-  })
-}
+  }),
+  methods: {
+    showInformation(index) {
+      let currentItem = this.$refs[`cardText${index}`][0];
+      if(currentItem.classList.contains('hidden')) {
+        currentItem.classList.remove('hidden')
+        return
+      }
+      currentItem.classList.add('hidden')
+    }
+  }
+};
 </script>
 <style scoped>
+.hidden {
+  display: none;
+}
 </style>
